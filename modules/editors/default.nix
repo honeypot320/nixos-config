271b13f6448e4bf07ba393c346cdd8e63ba3994e
honeypot320 <honeypot320@proton.me>
2026-04-18 21:52:47 +0200

    finnaly

--- modules/editors/default.nix
+++ modules/editors/default.nix
@@ -13,39 +13,39 @@
 
     configure = {
       customRC = ''
-        lua << EOF
-        vim.g.mapleader = " "
-        vim.g.maplocalleader = " "
+lua << EOF
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-        vim.opt.number = true
-        vim.opt.relativenumber = true
-        vim.opt.mouse = "a"
-        vim.opt.showmode = false
-        vim.opt.clipboard = "unnamedplus"
-        vim.opt.breakindent = true
-        vim.opt.undofile = true
-        vim.opt.ignorecase = true
-        vim.opt.smartcase = true
-        vim.opt.signcolumn = "yes"
-        vim.opt.updatetime = 250
-        vim.opt.timeoutlen = 300
-        vim.opt.splitright = true
-        vim.opt.splitbelow = true
-        vim.opt.list = true
-        vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-        vim.opt.inccommand = "split"
-        vim.opt.cursorline = true
-        vim.opt.scrolloff = 10
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
 
-	vim.keymap.set("n", "<leader>pv", ":Ex<CR>", { desc = "Open netrw" })
-	vim.keymap.set("n", "<leader>d", "ggdG")
+vim.keymap.set("n", "<leader>pv", ":Ex<CR>", { desc = "Open netrw" })
+vim.keymap.set("n", "<leader>d", "ggdG", { desc = "Delete whole file" })
 
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-        vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
-        EOF
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+EOF
       '';
 
       packages.myPlugins = with pkgs.vimPlugins; {
