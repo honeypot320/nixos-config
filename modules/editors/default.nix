fa59c89bf2e3a7342829f5838298cc0fc0e24bae
honeypot320 <honeypot320@proton.me>
2026-04-18 21:45:41 +0200

    feature: nvim shortcut added

--- modules/editors/default.nix
+++ modules/editors/default.nix
@@ -40,6 +40,7 @@
         vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" })
 	vim.keymap.set("n", "<leader>d", "ggdG")
 
+
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
