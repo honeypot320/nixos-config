28aafcb07f084ebbd266bf8cf438006f80314d98
honeypot320 <honeypot320@proton.me>
2026-04-18 20:57:13 +0200

    fix (hopefully)

--- modules/editors/default.nix
+++ modules/editors/default.nix
@@ -11,48 +11,54 @@
     withPython3 = true;
     withRuby = false;
 
+    configure = {
+      customRC = ''
+        lua << EOF
+        vim.g.mapleader = " "
+        vim.g.maplocalleader = " "
+
+        vim.opt.number = true
+        vim.opt.relativenumber = true
+        vim.opt.mouse = "a"
+        vim.opt.showmode = false
+        vim.opt.clipboard = "unnamedplus"
+        vim.opt.breakindent = true
+        vim.opt.undofile = true
+        vim.opt.ignorecase = true
+        vim.opt.smartcase = true
+        vim.opt.signcolumn = "yes"
+        vim.opt.updatetime = 250
+        vim.opt.timeoutlen = 300
+        vim.opt.splitright = true
+        vim.opt.splitbelow = true
+        vim.opt.list = true
+        vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+        vim.opt.inccommand = "split"
+        vim.opt.cursorline = true
+        vim.opt.scrolloff = 10
+
+        vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" })
+
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+        vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+        EOF
+      '';
+
+      packages.myPlugins = with pkgs.vimPlugins; {
+        start = [
+          plenary-nvim
+          telescope-nvim
+        ];
+      };
+    };
+
     extraPackages = with pkgs; [
       ripgrep
       fd
       gcc
     ];
-
-    plugins = with pkgs.vimPlugins; [
-      plenary-nvim
-      telescope-nvim
-    ];
-
-    extraLuaConfig = ''
-      vim.g.mapleader = " "
-      vim.g.maplocalleader = " "
-
-      vim.opt.number = true
-      vim.opt.relativenumber = true
-      vim.opt.mouse = "a"
-      vim.opt.showmode = false
-      vim.opt.clipboard = "unnamedplus"
-      vim.opt.breakindent = true
-      vim.opt.undofile = true
-      vim.opt.ignorecase = true
-      vim.opt.smartcase = true
-      vim.opt.signcolumn = "yes"
-      vim.opt.updatetime = 250
-      vim.opt.timeoutlen = 300
-      vim.opt.splitright = true
-      vim.opt.splitbelow = true
-      vim.opt.list = true
-      vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-      vim.opt.inccommand = "split"
-      vim.opt.cursorline = true
-      vim.opt.scrolloff = 10
-
-      vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" })
-
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
-    '';
   };
 }
